dbecd43ce4fc75b6372b12db35c3e69fff6e420d
Ordo <sam.peters24@gmail.com>
2023-11-28 23:45:59 +0000

    grid instantiation and fix, x and y were swapped

--- src/main.c
+++ src/main.c
@@ -1,6 +1,7 @@
 #include "raylib.h"
 #include <math.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 typedef enum {
     // using 0 for object makes the divergence calc easier
@@ -8,6 +9,7 @@ typedef enum {
     FLUID = 1,  // normal cell
 } ObjType;
 
+// should probably rename this to "side"
 typedef struct {
     float vel;
     ObjType obj_type;
@@ -16,21 +18,23 @@ typedef struct {
 // four pointers to each velocity component (sides).
 // edges are shared between Cells
 typedef struct {
-    float pressure;
+    // float pressure;
     Velocity* left;  // positive enters cell
     Velocity* right; // positive leaves cell
     Velocity* up;    // positive leaves cell
     Velocity* down;  // positive enters cell
 } GridCell;
 
+// should probably just use a rectangle struct
 typedef struct {
     int width;
     int height;
     // int depth; // for 3D
 } Size2D;
 
-void update(GridCell* grid[], Size2D size, int iterations);
-void draw(GridCell* grid[]);
+void update(Size2D size, GridCell grid[size.height][size.width], int iterations);
+// is it even worth putting this in a function?
+void draw(Texture2D* texture, Rectangle source, Rectangle dest, Vector2 origin);
 
 // All grid Cells i, j
 // add dt * g deltatime and -9.81 m/s
@@ -55,43 +59,85 @@ int main(void) {
     InitWindow(800, 450, "Fluid Dynamics Simulation");
     int grid_w = 20;
     int grid_h = 20;
-    GridCell grid[grid_w][grid_h];
+    Size2D grid_size = {grid_w, grid_h};
+    GridCell(*grid)[grid_w] = malloc(grid_w * grid_h * sizeof(GridCell));
     // instantiate grid
     // outer cells need to be objects
     // need to make (grid_w+1)*grid_h + grid_w*(grid_h+1) velcotiy "edges"
     int edge_count = (grid_w + 1) * grid_h + grid_w * (grid_h + 1);
-    // how the hell do i map these
-    //
-    // optimisation doesn't matter we only do this once (famous last words)
-    for (int x = 0; x < grid_w; x++) {
-        // x = 0 // top
-        // x = grid_h-1 // bottom
-        // y = 0 // left
-        // y = grid_w-1 // right
-        for (int y = 0; y < grid_h; y++) {
-            // this is hard
+
+    long vel_size = sizeof(Velocity);
+
+    for (int y = 0; y < grid_w; y++) {
+        for (int x = 0; x < grid_h; x++) {
+            // easier to read
+            GridCell* cell = &grid[y][x];
+            // turn this into a function
+            cell->down = malloc(vel_size);
+            cell->down->vel = x * y;
+            cell->down->obj_type = 1;
+            cell->right = malloc(vel_size);
+            cell->right->vel = x * y;
+            cell->right->obj_type = 1;
+
+            // this is extra jank
+            // top row
+            if (y == 0) {
+                cell->up = malloc(vel_size);
+                cell->up->vel = 0;
+                cell->up->obj_type = 0;
+            } else {
+                cell->up = grid[y - 1][x].down;
+            }
+            // left column
+            if (x == 0) {
+                cell->left = malloc(vel_size);
+                cell->left->vel = 0;
+                cell->left->obj_type = 0;
+            } else {
+                cell->left = grid[y][x - 1].right;
+            }
+            // bottom row
+            if (y == grid_h - 1) {
+                cell->down->obj_type = 0;
+                cell->down->vel = 0;
+            }
+            // right column
+            if (x == grid_w - 1) {
+                cell->right->obj_type = 0;
+                cell->right->vel = 0;
+            }
         }
     }
     int iterations = 2;
-    while (!WindowShouldClose()) {
-    }
+    Rectangle source = {0, 0, grid_w, grid_h};
+    Rectangle dest = {0, 0, GetScreenWidth(), GetScreenHeight()};
+    Image image = GenImageColor(grid_w, grid_h, WHITE);
 
+    Vector2 origin = {0, 0};
+    while (!WindowShouldClose()) {
+        // update(grid_size, grid, 1);
+        // probably bad
+        Texture2D texture = LoadTextureFromImage(blank);
+        draw(texture, source, dest, origin);
+        UnloadTexture(texture);
+    }
     CloseWindow();
 
     return 0;
 }
 
-void update(GridCell* grid[], Size2D size, int iterations) {
+void update(Size2D size, GridCell grid[size.height][size.width], int iterations) {
     const float relaxation = 1.5; // const between 1 and 2
     for (int i = 0; i < iterations; i++) {
-        for (int x = 0; x < size.width; x++) {
-            for (int y = 0; y < size.height; y++) {
-                GridCell* cell = &grid[x][y];
+        for (int y = 0; y < size.width; y++) {
+            for (int x = 0; x < size.height; x++) {
+                GridCell* cell = &grid[y][x];
                 // cell->pressure = 0; // do we need this?
 
                 // use this to colour cells?
                 float divergence = cell->right->vel - cell->left->vel + cell->up->vel - cell->down->vel;
-                divergence *= relaxation;
+                divergence *= relaxation; // make it less bad
 
                 // these two vars factor in objects
                 float diverg_mod = cell->down->obj_type + cell->right->obj_type + cell->left->obj_type + cell->up->obj_type;
@@ -106,9 +152,9 @@ void update(GridCell* grid[], Size2D size, int iterations) {
     }
 }
 
-void draw(GridCell* grid[]) {
+void draw(Texture2D* texture, Rectangle source, Rectangle dest, Vector2 origin) {
     BeginDrawing();
     ClearBackground(BLACK);
-    // draw grid
+    DrawTexturePro(*texture, source, dest, origin, 0, WHITE);
     EndDrawing();
 }
